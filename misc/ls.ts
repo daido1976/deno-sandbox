271f518a497f74006a03562fcaad2571596de605
daido1976 <daido1976@gmail.com>
2023-09-28 10:02:36 +0900

    Make ls functional style

--- misc/ls.ts
+++ misc/ls.ts
@@ -1,32 +1,30 @@
 import { blue } from "https://deno.land/std@0.202.0/fmt/colors.ts";
 
-class FilePresenter {
-  #name: string;
-  #isDirectory: boolean;
+type DirectoryEntry = {
+  name: string;
+  isDirectory: boolean;
+};
 
-  constructor(name: string, isDirectory: boolean) {
-    this.#name = name;
-    this.#isDirectory = isDirectory;
-  }
-
-  toPretty(): string {
-    return this.#isDirectory ? blue(this.#name) : this.#name;
-  }
+function toPretty(entry: DirectoryEntry): string {
+  return entry.isDirectory ? blue(entry.name) : entry.name;
 }
 
-function getFilesFromDirectory(path: string): FilePresenter[] {
+function getEntriesFromDirectory(path: string): DirectoryEntry[] {
   return [...Deno.readDirSync(path)].map((entry) => {
-    return new FilePresenter(entry.name, entry.isDirectory);
+    return {
+      name: entry.name,
+      isDirectory: entry.isDirectory,
+    };
   });
 }
 
-function convertFilesToOutput(files: FilePresenter[]): string {
-  return files.map((x) => x.toPretty()).join("  ");
+function convertToOutput(entries: DirectoryEntry[]): string {
+  return entries.map(toPretty).join("  ");
 }
 
 if (import.meta.main) {
   const targetPath = Deno.args.length > 0 ? Deno.args[0] : Deno.cwd();
-  const files = getFilesFromDirectory(targetPath);
-  const output = convertFilesToOutput(files);
+  const entries = getEntriesFromDirectory(targetPath);
+  const output = convertToOutput(entries);
   console.log(output);
 }
