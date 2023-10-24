f5023ce3b7597f8f76a20038508a3992187f0940
daido1976 <daido1976@gmail.com>
2023-10-24 22:31:50 +0900

    Minimize jsx.tsx

--- misc/jsx.tsx
+++ misc/jsx.tsx
@@ -1,10 +1,9 @@
 /** @jsxImportSource npm:preact */
 import { renderToString } from "npm:preact-render-to-string";
 
-function handler(_req: Request): Response {
-  return new Response(renderToString(<div>Hello world!</div>), {
-    headers: { "content-type": "text/html" },
-  });
-}
-
-Deno.serve(handler);
+Deno.serve(
+  (_req: Request) =>
+    new Response(renderToString(<div>Hello, world!</div>), {
+      headers: { "content-type": "text/html" },
+    })
+);
