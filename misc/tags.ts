94b2666be02c8634fecd396e57215c3ee61d5932
daido1976 <daido1976@gmail.com>
2023-10-24 22:06:13 +0900

    Introduce In-source testing

--- misc/tags.ts
+++ misc/tags.ts
@@ -84,3 +84,55 @@ export class ApartmentKlass {
     );
   }
 }
+
+import { assertEquals } from "https://deno.land/std@0.146.0/testing/asserts.ts";
+import { describe, it } from "https://deno.land/std@0.146.0/testing/bdd.ts";
+
+describe("tags", () => {
+  it("tags if conditions are met", () => {
+    const apartment = {
+      metersToStation: 400,
+      ageOfBuilding: 5,
+      options: ["ShoeLocker", "AutoLock"],
+    };
+
+    assertEquals(buildTagsImperative(apartment), ["駅近", "セキュリティ充実"]);
+    assertEquals(buildTagsDeclarative(apartment), ["駅近", "セキュリティ充実"]);
+  });
+
+  it("do not tags, if the condition is not met", () => {
+    const apartment = {
+      metersToStation: 1200,
+      ageOfBuilding: 10,
+      options: ["ShoeLocker"],
+    };
+
+    assertEquals(buildTagsImperative(apartment), []);
+    assertEquals(buildTagsDeclarative(apartment), []);
+  });
+});
+
+describe("tags with class", () => {
+  it("tags if conditions are met", () => {
+    const apartment = {
+      metersToStation: 400,
+      ageOfBuilding: 5,
+      options: ["ShoeLocker", "AutoLock"],
+    };
+
+    assertEquals(new ApartmentKlass(apartment).getTags(), [
+      "駅近",
+      "セキュリティ充実",
+    ]);
+  });
+
+  it("do not tags, if the condition is not met", () => {
+    const apartment = {
+      metersToStation: 1200,
+      ageOfBuilding: 10,
+      options: ["ShoeLocker"],
+    };
+
+    assertEquals(new ApartmentKlass(apartment).getTags(), []);
+  });
+});
